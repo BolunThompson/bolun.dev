a6859bc16db1ccf8eb3f258538d5438f0b49e759
BolunThompson <BolunThompson@users.noreply.github.com>
2025-09-25 18:32:19 +0000

    deploy: ba2998040380d0a43af67dac7cf1be9b1197352e

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,13 @@
 { pkgs, lib, config, ... }: {
-  packages = [ pkgs.cobalt pkgs.gh pkgs.nodejs_23 ];
+  packages = [ pkgs.cobalt pkgs.gh pkgs.nodejs_24 ];
 
   languages = {
     rust.enable = true;
   };
 
+  processes = {
+    cobalt-serve.exec = "cobalt serve";
+  };
+
   # See full reference at https://devenv.sh/reference/options/
 }
-
